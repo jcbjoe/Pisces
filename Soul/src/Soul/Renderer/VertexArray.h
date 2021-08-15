235ed3d3319527161cc461a2242409b20b12338a
JoeGarlick <joegarlick28@hotmail.com>
2021-08-15 23:35:03 +0100

    Added: Some getters

--- Soul/src/Soul/Renderer/VertexArray.h
+++ Soul/src/Soul/Renderer/VertexArray.h
@@ -16,6 +16,9 @@ namespace Soul
 		virtual void AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer) = 0;
 		virtual void SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer) = 0;
 
+		virtual const std::vector<std::shared_ptr<VertexBuffer>>& GetVertexBuffers() const;
+		virtual const std::shared_ptr<IndexBuffer>& GetIndexBuffer() const = 0;
+
 		static VertexArray* Create();
 	};
 }
